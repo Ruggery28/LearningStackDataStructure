a8310454840590e6e47b423d4d9980ad5272354c
Ruggery <ruggeryalves@hotmail.com>
2026-09-17 11:38:57 +0100

    Explaining the code with my words, what each method and values represent.

--- src/main/java/com/mycompany/learningstack/ArrayStack.java
+++ src/main/java/com/mycompany/learningstack/ArrayStack.java
@@ -9,17 +9,19 @@ package com.mycompany.learningstack;
  * @author Ruggery
  */
 public class ArrayStack {
-    //creating the two
-    private int[] stackArray;
-    private int top;
+    //creating the two variables, one array and the top.
+    private int[] stackArray; //empty array and no size has been set up yet.
+    private int top; //the top could have been initialised here as well = -1.
 
+    //constructor to initialise the size and also the top
     public ArrayStack(int capacity) {
         stackArray = new int[capacity];
         top = -1; // Indicates stack is initially empty
     }
 
+    //method push that will receive the value that will be stored.
     public void push(int value) {
-        top++; 
-        stackArray[top] = value;
+        top++; //increasing the top, previous value + 1.
+        stackArray[top] = value; //it will add the value inside the index represented by the 'top' value.
     }
 }
