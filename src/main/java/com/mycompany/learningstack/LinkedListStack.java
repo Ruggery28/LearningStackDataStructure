b6dbaa254b26fa20dcdba66a9ced50948580a274
Ruggery <ruggeryalves@hotmail.com>
2026-09-24 12:49:18 +0100

    Creating all the methods inside the class, push, pop, peek and display, also testing it inside the main.

--- src/main/java/com/mycompany/learningstack/LinkedListStack.java
+++ src/main/java/com/mycompany/learningstack/LinkedListStack.java
@@ -25,12 +25,51 @@ public class LinkedListStack {
 
     private Node top; //this will be our variable for the stack
 
-    public LinkedListStack(Node top) {
+    public LinkedListStack() {
         this.top = null; //Also start as null
     }
 
-    
-    
-    
-    
+    public boolean isEmpty() {
+        return top == null; //is top is null return true
+    }
+
+    public void push(int value) {
+        Node node = new Node(value); //passing the value to the data
+        node.next = top; //making the next to be linked with top (still null)
+        top = node; //the top now will be the last node
+    }
+
+    public int pop() {
+        //check first if the stack is empty before trying to look into the data
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty!");
+        }
+        int holdValue = top.data; //save the top value inside the new variable
+        top = top.next; //updating top so it now hold the down value
+        return holdValue; //then, returning the value in case we need to undo that.
+    }
+
+    public int peek() {
+        //check first if the stack is empty before trying to look into the data
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty!");
+        }
+        return top.data; //it will display the top value in our stack.
+    }
+
+    public void display() {
+        //check first if the stack is empty before trying to look into the data
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty!");
+        }
+        
+        Node current = top; //a new object to hold the last top value
+        //while there is a value we go through the loop
+        while(current != null){
+            System.out.println(current.data + "--> "); //print the current top data
+            current = current.next; //now the current value will hold the previous next
+        }
+        System.out.println("Null.");
+    }
+
 }
